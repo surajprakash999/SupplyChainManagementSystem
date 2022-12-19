7702fcb4c1472c6fc08214cab3f17e57bc06c3eb
SurajPrakash <prakash.champ999@gmail.com>
2022-12-19 17:35:03 +0530

    Header Bar

--- src/main/java/com/example/supplychange17dec/HelloApplication.java
+++ src/main/java/com/example/supplychange17dec/HelloApplication.java
@@ -3,6 +3,11 @@ package com.example.supplychange17dec;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -10,8 +15,51 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    public static final int width =700, height = 600, headerBar=50;
+
+    Pane bodyPane = new Pane();
+
+    private GridPane headerBar(){
+        TextField searchText = new TextField();
+        Button searchButton = new Button("Search");
+
+        GridPane gridPane = new GridPane();
+        gridPane.add(searchText,0,0);
+        gridPane.add(searchButton,1,0);
+        return gridPane;
+    }
+
+    private GridPane loginPage(){
+        Label emailLabel = new Label("Email");
+        Label passwordLabel = new Label("Password");
+
+        TextField emailTextField = new TextField();
+        PasswordField passwordField = new PasswordField();
+
+        GridPane gridPane = new GridPane();
+
+        gridPane.add(emailLabel,0,0);
+        gridPane.add(emailTextField,1,0);
+        gridPane.add(passwordLabel,0,1);
+        gridPane.add(passwordField,1,1);
+
+        return gridPane;
+
+
+    }
+
     private Pane createContent(){
         Pane root = new Pane();
+        root.setPrefSize(width, height+headerBar);
+
+        bodyPane.setMinSize(width,height);
+        bodyPane.setTranslateY(headerBar);
+
+        bodyPane.getChildren().addAll(loginPage());
+
+        root.getChildren().addAll(headerBar(),bodyPane);
+
+
         return root;
     }
     @Override
