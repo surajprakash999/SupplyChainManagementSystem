ef785605216c1431bb4956bffe3383dba95aba4d
SurajPrakash <prakash.champ999@gmail.com>
2022-12-19 16:21:02 +0530

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.supplychange17dec {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.supplychange17dec to javafx.fxml;
+    exports com.example.supplychange17dec;
+}
